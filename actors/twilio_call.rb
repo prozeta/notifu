fd6c7d3046b16b3de5aee300aa1448197698d33e
Radek 'blufor' Slavicinsky <radek@blufor.cz>
2015-05-20 12:07:01 +0200

    twilio_call actor rewritten so that it directly accesses the API without intermediate python script

--- actors/twilio_call.rb
+++ actors/twilio_call.rb
@@ -2,19 +2,24 @@ module Notifu
   module Actors
     class TwilioCall < Notifu::Actor
 
+      require 'excon'
+      require 'erb'
+
       self.name = "twilio_call"
       self.desc = "POST requst to trigger phone-call"
       self.retry = 2
 
       def act
         contacts = self.contacts.map { |contact| contact.cell }
-        request = {
-          status: self.issue.code.to_status,
-          hostname: self.issue.host,
-          description: [self.issue.service.to_s, self.issue.message.to_s].join(': '),
-          call_group: "[#{contacts.join(',')}]"
-        }.to_json
-        exec(Notifu::CONFIG[:actors][:twilio_call][:script_path] + " <<< '#{request}'")
+        req_string = Notifu::CONFIG[:actors][:twilio_call][:api] +
+                      "?token="       + Notifu::CONFIG[:actors][:twilio_call][:token] +
+                      "&status="      + self.issue.code.to_state +
+                      "&hostname="    + self.issue.host +
+                      "&status="      + self.issue.service +
+                      "&description=" + ERB::Util.url_encode(self.issue.message.to_s) +
+                      "&call_group="  + ERB::Util.url_encode(contacts.to_json) +
+                      "&init=1"
+        Excon.get req_string if self.issue.code.to_i == 2
       end
 
     end
